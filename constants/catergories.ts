cb6d9330213c777c292e5fb5e8f4e33ac9787c8e
Francis Ong <francis.ong@stratpoint.com>
2023-05-13 23:03:12 +0800

    feat: add categories constants

---
+++ constants/catergories.ts
@@ -0,0 +1,24 @@
+import { TbBeach } from 'react-icons/tb';
+import { GiWindmill } from 'react-icons/gi';
+import { MdOutlineVilla } from 'react-icons/md';
+
+export const CATEGORIES = [
+  {
+    id: 'category-001',
+    label: 'Beach',
+    icon: TbBeach,
+    description: 'This property is close to the beach!',
+  },
+  {
+    id: 'category-002',
+    label: 'Windmills',
+    icon: GiWindmill,
+    description: 'This property has windmills!',
+  },
+  {
+    id: 'category-003',
+    label: 'Modern',
+    icon: MdOutlineVilla,
+    description: 'This property has modern design!',
+  },
+];
