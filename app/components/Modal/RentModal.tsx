31f83d8ce033032e1280dc800beb95c759f3d9b6
Francis Ong <francis.ong@stratpoint.com>
2023-05-16 15:35:39 +0800

    refactor: rename hook extensions and create country select component

--- app/components/Modal/RentModal.tsx
+++ app/components/Modal/RentModal.tsx
@@ -1,16 +1,19 @@
 'use client';
 
+import toast from 'react-hot-toast';
 import { useCallback, useMemo, useState } from 'react';
+import { useForm, FieldValues, SubmitHandler } from 'react-hook-form';
 import { RENT_STEPS } from '@/constants/rent-modal-steps';
 import { CATEGORIES } from '@/constants/categories';
-import { useForm, FieldValues } from 'react-hook-form';
 import useRentModal from '@/app/hooks/useRentModal';
 
 import Modal from './index';
 import Heading from '../UI/Heading';
 import CategoryInput from '../UI/Input/CategoryInput';
+import CountrySelect from '../UI/Input/CountrySelect';
 
 const RentModal = () => {
+  const [isLoading, setLoading] = useState(false);
   const rentModal = useRentModal();
   const [step, setStep] = useState(RENT_STEPS.CATEGORY);
 
@@ -71,6 +74,31 @@ const RentModal = () => {
     return 'Back';
   }, [step]);
 
+  const onSubmit: SubmitHandler<FieldValues> = useCallback(
+    async (data) => {
+      setLoading(true);
+      console.log(data);
+
+      try {
+        const response = await fetch('submit rent');
+
+        if (response?.ok) {
+          toast.success('Log in success');
+          rentModal.onClose();
+        }
+
+        // if (response?.error) {
+        //   toast.error('Invalid email or password');
+        // }
+      } catch (error) {
+        toast.error('Something went wrong');
+      } finally {
+        setLoading(false);
+      }
+    },
+    [rentModal]
+  );
+
   /* Rent Modal Contents */
   let bodyContent = (
     <div className="flex flex-col gap-8">
@@ -99,13 +127,38 @@ const RentModal = () => {
     </div>
   );
 
+  if (step === RENT_STEPS.LOCATION) {
+    bodyContent = (
+      <div className="flex flex-col gap-8">
+        <Heading title="Where is your place located?" subTitle="Help guests find you!" center />
+        <CountrySelect />
+      </div>
+    );
+  }
+
+  if (step === RENT_STEPS.INFO) {
+    bodyContent = <div>Info Step</div>;
+  }
+
+  if (step === RENT_STEPS.IMAGES) {
+    bodyContent = <div>Images Step</div>;
+  }
+
+  if (step === RENT_STEPS.DESCRIPTION) {
+    bodyContent = <div>Description Step</div>;
+  }
+
+  if (step === RENT_STEPS.PRICE) {
+    bodyContent = <div>Price Step</div>;
+  }
+
   return (
     <Modal
       title="Staycify your home"
       actionLabel={actionLabel}
       isOpen={rentModal.isOpen}
       onClose={rentModal.onClose}
-      onSubmit={rentModal.onClose} // will change later
+      onSubmit={step === RENT_STEPS.PRICE ? handleSubmit(onSubmit) : onNext}
       secondaryAction={step === RENT_STEPS.CATEGORY ? undefined : onPrev}
       secondaryActionLabel={secondaryActionLabel}
       body={bodyContent}
