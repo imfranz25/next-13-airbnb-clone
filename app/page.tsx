a0b1c88b676c831e073b0c188c2a5ca431439007
Francis Ong <francis.ong@stratpoint.com>
2023-05-19 09:31:46 +0800

    feat: add get Listing helper func

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,20 @@
-'use client';
-
-import { useState } from 'react';
-
 import Container from './components/UI/Container';
 import EmptyState from './components/EmptyState';
+import { getListings } from './helpers/getListings';
 
-export default function Home() {
-  const [isEmpty, setEmpty] = useState(true);
+export default async function Home() {
+  const listings = await getListings();
 
-  if (isEmpty) {
+  if (listings.length === 0) {
     return <EmptyState showReset />;
   }
 
   return (
     <Container>
       <div className="grid grid-cols-1 gap-8 pt-24 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
-        <div>My future listings</div>
+        {listings.map((listing) => (
+          <div key={listing.id}>{listing.title}</div>
+        ))}
       </div>
     </Container>
   );
