cea83b6ab8de96c4b051b0515f9726e8b9e622a0
Francis Ong <francis.ong@stratpoint.com>
2023-05-20 14:20:19 +0800

    feat: use listing component to listing client

--- app/listings/[listingId]/ListingClient.tsx
+++ app/listings/[listingId]/ListingClient.tsx
@@ -6,11 +6,12 @@ import { Reservation } from '@prisma/client';
 
 import Container from '@/app/components/UI/Container';
 import ListingHead from '@/app/components/Listings/ListingHead';
+import ListingInfo from '@/app/components/Listings/ListingInfo';
 import { CATEGORIES } from '../../../constants/categories';
 
 interface ListingClientProps {
   listing: SafeListing & {
-    user?: SafeUser;
+    user: SafeUser;
   };
   currentUser?: SafeUser | null;
   reservation?: Reservation[];
@@ -23,7 +24,7 @@ const ListingClient: React.FC<ListingClientProps> = ({ listing, currentUser }) =
 
   return (
     <Container>
-      <div className="max-w-screen-lg mx-auto my-2">
+      <div className="max-w-screen-lg mx-auto my-10">
         <div className="flex flex-col gap-6">
           <ListingHead
             title={listing.title}
@@ -32,6 +33,17 @@ const ListingClient: React.FC<ListingClientProps> = ({ listing, currentUser }) =
             id={listing.id}
             currentUser={currentUser}
           />
+          <div className="grid grid-cols-1 my-6 md:grid-cols-7 md:gap-100">
+            <ListingInfo
+              user={listing.user}
+              category={category}
+              description={listing.description}
+              roomCount={listing.roomCount}
+              guestCount={listing.guestCount}
+              bathroomCount={listing.bathroomCount}
+              locationValue={listing.locationValue}
+            />
+          </div>
         </div>
       </div>
     </Container>
