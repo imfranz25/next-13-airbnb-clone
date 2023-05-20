60747c14d7a66ab28d59c3767bc26545a8cd8d31
Francis Ong <francis.ong@stratpoint.com>
2023-05-20 08:37:18 +0800

    feat: add useFavorite hook

---
+++ app/hooks/useFavorites.ts
@@ -0,0 +1,61 @@
+import axios from 'axios';
+import { useRouter } from 'next/navigation';
+import React, { use, useCallback, useMemo } from 'react';
+import toast from 'react-hot-toast';
+
+import { SafeUser } from '../types';
+
+import useLoginModal from './useLoginModal';
+
+interface IUseFavorite {
+  listingId: string;
+  currentUser?: SafeUser | null;
+}
+
+const useFavorite = ({ listingId, currentUser }: IUseFavorite) => {
+  const router = useRouter();
+  const loginModal = useLoginModal();
+
+  const hasFavorited = useMemo(() => {
+    const list = currentUser?.favoriteIds || [];
+
+    return list.includes(listingId);
+  }, [currentUser, listingId]);
+
+  const toggleFavorite = useCallback(
+    async (event: React.MouseEvent<HTMLDivElement>) => {
+      event.stopPropagation();
+
+      if (!currentUser) {
+        return loginModal.onOpen();
+      }
+
+      try {
+        let request;
+        let toggleMessage;
+
+        if (hasFavorited) {
+          request = axios.delete(`/api/favorite/${listingId}`);
+          toggleMessage = 'Added to favorites';
+        } else {
+          request = axios.post(`/api/favorite/${listingId}`);
+          toggleMessage = 'Removed from favorites';
+        }
+
+        await request;
+        router.refresh();
+        toast.success(toggleMessage);
+      } catch (error) {
+        toast.success('Something went wrong');
+      }
+    },
+    [currentUser, loginModal, router, listingId, hasFavorited]
+  );
+
+  return {
+    hasFavorited,
+    toggleFavorite,
+  };
+};
+
+export default useFavorite;
