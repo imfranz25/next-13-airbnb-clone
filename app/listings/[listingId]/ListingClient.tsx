4f14694bcfeb3a553b3b052b7eb8f18ceee17605
Francis Ong <francis.ong@stratpoint.com>
2023-05-20 17:20:21 +0800

    feat: add reservation func states and calendar date picker

--- app/listings/[listingId]/ListingClient.tsx
+++ app/listings/[listingId]/ListingClient.tsx
@@ -1,6 +1,10 @@
 'use client';
 
-import React, { useMemo } from 'react';
+import axios from 'axios';
+import toast from 'react-hot-toast';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
+import { differenceInCalendarDays, eachDayOfInterval } from 'date-fns';
+import { useRouter } from 'next/navigation';
 import { SafeListing, SafeUser } from '@/app/types';
 import { Reservation } from '@prisma/client';
 
@@ -8,19 +12,98 @@ import Container from '@/app/components/UI/Container';
 import ListingHead from '@/app/components/Listings/ListingHead';
 import ListingInfo from '@/app/components/Listings/ListingInfo';
 import { CATEGORIES } from '../../../constants/categories';
+import useLoginModal from '@/app/hooks/useLoginModal';
+
+const initialDateRange = {
+  startDate: new Date(),
+  endDate: new Date(),
+  key: 'selection',
+};
 
 interface ListingClientProps {
   listing: SafeListing & {
     user: SafeUser;
   };
   currentUser?: SafeUser | null;
-  reservation?: Reservation[];
+  reservations?: Reservation[];
 }
 
-const ListingClient: React.FC<ListingClientProps> = ({ listing, currentUser }) => {
+const ListingClient: React.FC<ListingClientProps> = ({
+  listing,
+  currentUser,
+  reservations = [],
+}) => {
+  const router = useRouter();
+  const loginModal = useLoginModal();
+  const [isLoading, setLoading] = useState(false);
+  const [totalPrice, setTotalPrice] = useState(listing.price);
+  const [dateRange, setDateRange] = useState(initialDateRange);
+
+  const onCreateReservation = useCallback(async () => {
+    if (!currentUser) {
+      return loginModal.onOpen();
+    }
+
+    setLoading(true);
+
+    try {
+      await axios.post('/api/reservations', {
+        totalPrice,
+        startDate: dateRange.startDate,
+        endDate: dateRange.endDate,
+        listingId: listing?.id,
+      });
+
+      toast.success('Listing reserved!');
+      router.refresh();
+      setDateRange(initialDateRange);
+
+      // TODO: REDIRECT
+    } catch (error) {
+      toast.error('Something went wrong');
+    } finally {
+      setLoading(false);
+    }
+  }, [
+    currentUser,
+    loginModal,
+    totalPrice,
+    dateRange.startDate,
+    dateRange.endDate,
+    listing.id,
+    router,
+  ]);
+
+  const disabledDate = useMemo(() => {
+    let dates: Date[] = [];
+
+    reservations.forEach((reservation) => {
+      const range = eachDayOfInterval({
+        start: new Date(reservation.startDate),
+        end: new Date(reservation.endDate),
+      });
+
+      dates = [...dates, ...range];
+    });
+
+    return dates;
+  }, [reservations]);
+
   const category = useMemo(() => {
     return CATEGORIES.find((item) => item.label === listing.category);
   }, [listing.category]);
+
+  useEffect(() => {
+    if (dateRange.startDate && dateRange.endDate) {
+      const daysCount = differenceInCalendarDays(dateRange.endDate, dateRange.startDate);
+
+      if (daysCount && listing.price) {
+        setTotalPrice(daysCount * listing.price);
+      } else {
+        setTotalPrice(listing.price);
+      }
+    }
+  }, [dateRange.endDate, dateRange.startDate, listing.price]);
 
   return (
     <Container>
