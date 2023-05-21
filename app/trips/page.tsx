fa2b359240d5f75ac54907323ead36d6b7778ac6
Francis Ong <francis.ong@stratpoint.com>
2023-05-21 15:23:05 +0800

    feat: add delete/cancel reservation api

--- app/trips/page.tsx
+++ app/trips/page.tsx
@@ -13,9 +13,9 @@ const page = async () => {
 
   const reservations = await getReservations({ userId: currentUser.id });
 
-  if (!reservations) {
+  if (reservations.length === 0) {
     return (
-      <EmptyState title="No trips found" subtitle="Looks like you haven&'t reserved any trips" />
+      <EmptyState title="No trips found" subtitle="Looks like you haven't reserved any trips" />
     );
   }
 
