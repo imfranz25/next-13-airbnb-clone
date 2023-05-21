43177c061f2afa4b98d470da81d2e73ac6a1c022
Francis Ong <francis.ong@stratpoint.com>
2023-05-21 21:29:18 +0800

    feat: add fav pagge client

--- app/favorites/FavoritesClient.tsx
+++ app/favorites/FavoritesClient.tsx
@@ -2,13 +2,26 @@
 
 import { SafeListing, SafeUser } from '../types';
 
+import Container from '../components/UI/Container';
+import Heading from '../components/UI/Heading';
+import ListingCard from '../components/Listings/ListingCard';
+
 interface FavoritesClientProps {
   listings?: SafeListing[];
   currentUser?: SafeUser | null;
 }
 
 const FavoritesClient: React.FC<FavoritesClientProps> = ({ listings, currentUser }) => {
-  return <div>FavoritesClient</div>;
+  return (
+    <Container>
+      <Heading title="Favorites" subTitle="List of place you have marked as favorite" center />
+      <div className="grid grid-cols-1 mt-10 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
+        {listings?.map((listing) => (
+          <ListingCard key={listing.id} data={listing} currentUser={currentUser} />
+        ))}
+      </div>
+    </Container>
+  );
 };
 
 export default FavoritesClient;
