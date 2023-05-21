c59c986b7970bb8a727253a3edd32fd11823ab26
Francis Ong <francis.ong@stratpoint.com>
2023-05-21 21:37:35 +0800

    feat: add gap between cards

--- app/favorites/FavoritesClient.tsx
+++ app/favorites/FavoritesClient.tsx
@@ -15,7 +15,7 @@ const FavoritesClient: React.FC<FavoritesClientProps> = ({ listings, currentUser
   return (
     <Container>
       <Heading title="Favorites" subTitle="List of place you have marked as favorite" center />
-      <div className="grid grid-cols-1 mt-10 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
+      <div className="grid grid-cols-1 gap-8 mt-10 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
         {listings?.map((listing) => (
           <ListingCard key={listing.id} data={listing} currentUser={currentUser} />
         ))}
