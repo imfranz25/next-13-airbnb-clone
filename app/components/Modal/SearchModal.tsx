3b60f2903d93c3d9a506ae70d4076d8e517d19ad
Francis Ong <francis.ong@stratpoint.com>
2023-05-26 16:25:06 +0800

    feat: add searcch modal func filters

--- app/components/Modal/SearchModal.tsx
+++ app/components/Modal/SearchModal.tsx
@@ -1,14 +1,19 @@
 'use client';
 
-import useSearchModal from '@/app/hooks/useSearchModal';
 import { useCallback, useMemo, useState } from 'react';
 import { useParams, useRouter } from 'next/navigation';
-import { SEARCH_STEPS } from '@/constants/search-modal-steps';
 import { Range } from 'react-date-range';
+import { formatISO } from 'date-fns';
+import dynamic from 'next/dynamic';
+import queryString from 'query-string';
+import useSearchModal from '@/app/hooks/useSearchModal';
 
 import Modal from '.';
-import dynamic from 'next/dynamic';
-import { CountrySelectValue } from '../UI/Input/CountrySelect';
+import Heading from '../UI/Heading';
+import CountrySelect, { CountrySelectValue } from '../UI/Input/CountrySelect';
+import Calendar from '../UI/Input/Calendar';
+import Counter from '../UI/Input/Counter';
+import { SEARCH_STEPS } from '@/constants/search-modal-steps';
 
 const initialDateRange = {
   startDate: new Date(),
@@ -30,24 +35,149 @@ const SearchModal = () => {
 
   const Map = useMemo(
     () =>
-      dynamic(import('../Map'), {
+      dynamic(() => import('../Map'), {
         ssr: false,
       }),
     // eslint-disable-next-line react-hooks/exhaustive-deps
     [location]
   );
 
-  const onSubmitSearch = useCallback(() => {}, []);
+  const onNext = useCallback(() => {
+    setStep((value) => value + 1);
+  }, []);
+
+  const onBack = useCallback(() => {
+    setStep((value) => value - 1);
+  }, []);
+
+  const onSubmitSearch = useCallback(async () => {
+    if (step !== SEARCH_STEPS.INFO) {
+      return onNext();
+    }
+
+    let currentQuery = {};
+
+    if (params) {
+      currentQuery = queryString.parse(params.toString());
+    }
+
+    const finalQuery: any = {
+      ...currentQuery,
+      locationValue: location?.value,
+      guestCount,
+      roomCount,
+      bathroomCount,
+    };
+
+    if (dateRange.startDate) {
+      finalQuery.startDate = formatISO(dateRange.startDate);
+    }
+
+    if (dateRange.endDate) {
+      finalQuery.endDate = formatISO(dateRange.endDate);
+    }
+
+    const url = queryString.stringifyUrl(
+      {
+        url: '/',
+        query: finalQuery,
+      },
+      { skipNull: true }
+    );
+
+    setStep(SEARCH_STEPS.LOCATION);
+    searchModal.onClose();
+
+    router.push(url);
+  }, [
+    step,
+    onNext,
+    params,
+    location,
+    guestCount,
+    roomCount,
+    bathroomCount,
+    dateRange,
+    searchModal,
+    router,
+  ]);
+
+  const actionLabel = useMemo(() => {
+    if (step === SEARCH_STEPS.INFO) {
+      return 'Search';
+    }
+
+    return 'Next';
+  }, [step]);
+
+  const secondaryActionLabel = useMemo(() => {
+    if (step === SEARCH_STEPS.LOCATION) {
+      return undefined;
+    }
+
+    return 'Back';
+  }, [step]);
+
+  let bodyContent = (
+    <div className="flex flex-col gap-8">
+      <Heading center title="Where do you wanna go?" subTitle="Find the perfect location" />
+      <CountrySelect
+        value={location}
+        onChange={(value) => setLocation(value as CountrySelectValue)}
+      />
+      <hr />
+      <Map center={location?.latlng} />
+    </div>
+  );
+
+  if (step === SEARCH_STEPS.DATE) {
+    bodyContent = (
+      <div className="flex flex-col gap-8">
+        <Heading center title="When do you plan to go?" subTitle="Make sure everyone is free" />
+        <Calendar value={dateRange} onChange={(value) => setDateRange(value.selection)} />
+      </div>
+    );
+  }
+
+  if (step === SEARCH_STEPS.INFO) {
+    bodyContent = (
+      <div className="flex flex-col gap-8">
+        <Heading center title="More information" subTitle="Find your perfect place" />
+        <Counter
+          title="Guest"
+          subtitle="How many guest are coming?"
+          value={guestCount}
+          onChange={(value) => setGuestCount(value)}
+        />
+        <Counter
+          title="Rooms"
+          subtitle="How many rooms do you need?"
+          value={roomCount}
+          onChange={(value) => setRoomCount(value)}
+        />
+        <Counter
+          title="Bathrooms"
+          subtitle="How many bathrooms do you prefer?"
+          value={bathroomCount}
+          onChange={(value) => setBathroomCount(value)}
+        />
+      </div>
+    );
+  }
 
   return (
     <Modal
+      body={bodyContent}
       isOpen={searchModal.isOpen}
       onClose={searchModal.onClose}
       onSubmit={onSubmitSearch}
+      actionLabel={actionLabel}
+      secondaryActionLabel={secondaryActionLabel}
+      secondaryAction={step === SEARCH_STEPS.LOCATION ? undefined : onBack}
       title="Filters"
-      actionLabel="Search"
     />
   );
 };
 
 export default SearchModal;
+``;
