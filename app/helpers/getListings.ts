d397a0d0cace02a2fe10277a0021cabf383e9c53
Francis Ong <francis.ong@stratpoint.com>
2023-05-26 18:13:57 +0800

    feat: handle filters in getListings

--- app/helpers/getListings.ts
+++ app/helpers/getListings.ts
@@ -2,15 +2,70 @@ import prisma from '@/app/libs/prismadb';
 
 export interface IListingParams {
   userId?: string;
+  guestCount?: number;
+  roomCount?: number;
+  bathroomCount?: number;
+  startDate?: string;
+  endDate?: string;
+  category?: string;
+  locationValue?: string;
 }
 
 export default async function getListings(params: IListingParams) {
   try {
-    const { userId } = params;
     const query: any = {};
+    const {
+      userId,
+      guestCount,
+      roomCount,
+      bathroomCount,
+      startDate,
+      endDate,
+      category,
+      locationValue,
+    } = params;
 
     if (userId) {
       query.userId = userId;
+    }
+
+    if (guestCount) {
+      query.guestCount = {
+        gte: +guestCount,
+      };
+    }
+
+    if (roomCount) {
+      query.roomCount = {
+        gte: +roomCount,
+      };
+    }
+
+    if (bathroomCount) {
+      query.bathroomCount = {
+        gte: +bathroomCount,
+      };
+    }
+
+    if (category) {
+      query.category = category;
+    }
+
+    if (locationValue) {
+      query.locationValue = locationValue;
+    }
+
+    if (startDate && endDate) {
+      query.NOT = {
+        reservations: {
+          some: {
+            OR: [
+              { startDate: { gte: startDate }, endDate: { lte: endDate } },
+              { startDate: { lte: endDate }, endDate: { gte: startDate } },
+            ],
+          },
+        },
+      };
     }
 
     const listings = await prisma.listing.findMany({
